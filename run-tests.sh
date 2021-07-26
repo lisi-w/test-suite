0e1ff416db50ad50f342a625fc7572c48345f1e6
Elysia Witham <e.witham@columbia.edu>
2021-07-26 12:50:12 -0400

    streamlined testing and error checking

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,27 @@
 wd=$(pwd)
-bash map_test.sh
-bash input4mips-workflow.sh $wd/input4mips/workflow
-bash individual_tests.sh
+mkdir $wd/tmp
+LOGDIR=$wd/tmp
+check_exit () {
+  LOG=$1
+  ERRS=$(cat $LOG | grep error | wc -l)
+  if [ $ERRS -gt 0 ]
+  then
+    echo "ERROR encountered. See $LOG for details. Exiting."
+    exit $2
+  else
+    echo "SUCCESS"
+  fi
+}
+echo "Reseting index"
+bash reset_index.sh > $LOGDIR/reset-index.log 2>&1
+check_exit $LOGDIR/reset-index.log 1
+echo "Testing separate mapfile integrations and most projects"
+bash map_test.sh > $LOGDIR/map-test.log 2>&1
+check_exit $LOGDIR/map-test.log 2
+echo "Testing input4MIPs mapfiles"
+bash input4mips-workflow.sh $wd/input4mips/workflow > $LOGDIR/input4mips.log 2>&1
+check_exit $LOGDIR/input4mips.log 3
+echo "Testing individual modules"
+bash individual_tests.sh > $LOGDIR/modules.log 2>&1
+check_exit $LOGDIR/modules.log 4
+rm -rf $wd/tmp 
